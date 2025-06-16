d1249da98e537390da916a9e46e2c04b376d4b4c
João Daniel B Bernardino de lima <122681503+DanielDya@users.noreply.github.com>
2025-06-16 16:00:34 -0300

    Update dados.sql

--- database/dados.sql
+++ database/dados.sql
@@ -17,3 +17,9 @@ CREATE TABLE adocoes (
   adotante_email VARCHAR(100) NOT NULL,
   adotante_telefone VARCHAR(30) NOT NULL
 );
+
+INSERT INTO pets (nome, idade, genero, descricao, imagem, status) VALUES
+('Luna', '2 anos', 'Fêmea', 'Gata dócil e carinhosa, adora brincar com bolinhas', 'https://i.pinimg.com/736x/91/57/d8/9157d8c73ecc5561598536047bb41ed9.jpg', 'disponivel'),
+('Thor', '1 ano', 'Macho', 'Gato brincalhão e energético, perfeito para famílias ativas', 'https://i.pinimg.com/736x/86/80/f3/8680f3c873a4e2487ae5db40f38762bf.jpg', 'disponivel'),
+('Mia', '3 anos', 'Fêmea', 'Gata tranquila e independente, ideal para apartamentos', 'https://i.pinimg.com/736x/b4/17/6c/b4176cf53928d8d883270b9c5bfa0005.jpg', 'disponivel'),
+('Simba', '6 meses', 'Macho', 'Filhote muito brincalhão e carinhoso', 'https://i.pinimg.com/736x/80/80/b7/8080b78d7fc2d24c274f91bd436e8f1a.jpg', 'disponivel');
